982af39f0a7ba11597e58f2d9906824f609f02e1
Viktor Ciolkowski <wikcioo@gmail.com>
2022-12-26 18:34:04 +0100

    Display prompt and get user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let input = get_user_input();
+        parse_user_input(input);
+    }
+}
+
+fn parse_user_input(input: String) {
+    println!("You typed: {input}");
+}
+
+fn get_user_input() -> String {
+    let mut input = String::new();
+    print!("[virt-manager]\x1b[32m#\x1b[0m ");
+
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input.trim().to_string()
 }
