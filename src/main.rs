a9fc1087b9142ea669d4de31b4c6e9186ab73bc9
Viktor Ciolkowski <wikcioo@gmail.com>
2022-12-27 19:04:08 +0100

    Add new commands to be parsed
    Includes 'help' to print usage, 'version' to get the program version,
    'quit' to quit the program, '' is ignored, and otherwise print command
    error.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
 use std::io::{self, Write};
+use std::process::exit;
+
+const VERSION: &str = "0.1.0";
 
 fn main() {
     loop {
         let input = get_user_input();
-        parse_user_input(input);
+        parse_user_input(&input);
     }
 }
 
-fn parse_user_input(input: String) {
-    println!("You typed: {input}");
+fn parse_user_input(input: &str) {
+    match input {
+        "help" => {
+            print_usage();
+        }
+        "version" => {
+            println!("Version: {}", VERSION);
+        }
+        "quit" => {
+            exit(0);
+        }
+        "" => {
+            return;
+        }
+        _ => {
+            println!("Command '{input}' is not supported!");
+            return;
+        }
+    }
 }
 
 fn get_user_input() -> String {
@@ -21,4 +41,19 @@ fn get_user_input() -> String {
         .expect("Failed to read line");
 
     input.trim().to_string()
+}
+
+fn print_usage() {
+    println!("virt-manager-cli {VERSION}");
+    println!("Virtual machines manager command line utility");
+    println!();
+    println!("USAGE:");
+    println!("  virt-manager-cli [FLAGS] [OPTIONS] ARGUMENTS");
+    println!();
+    println!("FLAGS:");
+    println!("  -h, --help       Prints help information");
+    println!("  -v, --version    Prints version information");
+    println!();
+    println!("OPTIONS:");
+    println!("  -i, --interactive    Starts the interactive mode");
 }
