75fd9fbd0211723bfac2dee039979bb2babe5c1b
Viktor Ciolkowski <wikcioo@gmail.com>
2023-01-09 22:36:22 +0100

    Create a program directory at start-up if it doesn't exist

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::env;
+use std::fs;
 use std::io::{self, Write};
+use std::path::{Path, PathBuf};
 use std::process::exit;
 
 const VERSION: &str = "0.1.0";
+const PROGRAM_DIR_NAME: &str = ".virt-manager";
 
 fn main() {
+    init_program_directory();
+
     loop {
         let input = get_user_input();
         parse_user_input(&input);
@@ -16,19 +22,50 @@ fn parse_user_input(input: &str) {
             print_usage();
         }
         "version" => {
-            println!("Version: {}", VERSION);
+            println!("Version: {VERSION}");
         }
         "quit" => {
             exit(0);
         }
-        "" => {
-            return;
-        }
+        "" => {}
         _ => {
             println!("Command '{input}' is not supported!");
-            return;
         }
     }
+}
+
+fn init_program_directory() {
+    let program_directory_abs_path = get_program_directory_abs_path();
+
+    let program_directory = Path::new(&program_directory_abs_path);
+    if !program_directory.exists() {
+        create_program_directory(&program_directory_abs_path);
+    }
+}
+
+fn get_program_directory_abs_path() -> String {
+    let home_dir = match env::var("HOME") {
+        Ok(val) => val,
+        Err(e) => panic!("Failed to get HOME directory: {e}"),
+    };
+
+    let mut prog_path = PathBuf::new();
+    prog_path.push(home_dir);
+    prog_path.push(PROGRAM_DIR_NAME);
+
+    if let Some(path) = prog_path.to_str() {
+        String::from(path)
+    } else {
+        panic!("Failed to get program directory!");
+    }
+}
+
+fn create_program_directory(path: &str) {
+    if let Err(e) = fs::create_dir(path) {
+        eprintln!("Error creating directory: {e}");
+    }
+
+    println!("Created program directory");
 }
 
 fn get_user_input() -> String {
